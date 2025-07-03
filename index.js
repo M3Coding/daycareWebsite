8eb55a2f3b803c0f9426804168080313706e9c87
M3Coding <meady2009@gmail.com>
2025-07-03 17:56:02 -0400

    Frontend facing blog page complete

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
-app.use(bodyParser.urlencoded({ extended: true }));
+let posts = [];
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -21,7 +24,42 @@ app.get('/career', (req, res) => {
 app.get('/enrollment', (req, res) => {
   res.render("enrollment.ejs");
 });
+app.get('/teacherBlog', (req, res) => {
+  res.render("teacherBlog.ejs", {posts: posts});
+});
 
+app.post("/submit", (req, res) =>{
+    var today = new Date()
+    const newPost= {
+        id: Date.now(),
+        content: req.body.userPost,
+        today: today,
+        blogTitle: req.body.blogTitle,
+        userName:req.body.userName
+
+        };
+        posts.push(newPost);
+    
+    res.redirect("/teacherBlog");
+})
+app.post("/update", (req, res) => {
+  const { id, content } = req.body;
+  const post = posts.find(p => p.id == id);
+  if (post) {
+    post.content = content;
+  }
+  res.sendStatus(200);
+});
+app.delete("/update", (req, res) => {
+    const {id} = req.body;
+  const index = posts.findIndex(p => p.id == id);
+
+  if (index !== -1) {
+    posts.splice(index, 1)
+  }
+  res.sendStatus(200);
+    
+})//this delete block of code req the id of the post. The array is indexed to find that id and if it matches the number then it deletes with the splice function.
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
